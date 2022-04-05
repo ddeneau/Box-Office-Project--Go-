f0273b829823564b75e086879776bec7996f7373
Danny <ddeneau@terpmail.umd.edu>
2022-04-05 00:51:58 -0400

    Made some slight progress on basic functionality.
    Working:
    - Connects to API
    - Gathers random movies
    - Writing to CSV file
    Need to Improve:
    - randomness
    - data re-structuring.

--- boxOffice.go
+++ boxOffice.go
@@ -1,41 +1,80 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
+	"os"
 )
 
-// tt0103285 IMDB ID - Sample
+//tt0103285 //IMDB ID - Sample
 // My Key : 6110282
 
-func connectAndCollect() {
+var titles [100][1]string
+
+func findTitles() {
+	currentID := ""
+	batchSize := 100
+	
+	for i := 0; i < batchSize; i++ {
+		n1, n2 := rand.Intn(9), rand.Intn(9)
+		n3, n4 := rand.Intn(9), rand.Intn(9)
+		n5 := rand.Intn(9)	
+		currentID = fmt.Sprintf("tt0%d0%d%d%d%d", n1, n2, n3, n4, n5)
+		connectAndCollect(currentID, i)
+	}
+
+	fmt.Printf(titles[0][0])
+}
+
+func connectAndCollect(id string, index int) {
 	movieMap := make(map[string]string) // Create Map
 
-	url := "http://www.omdbapi.com/?t=joker&apikey=6110282" // Establish URL
+	url := fmt.Sprintf("http://www.omdbapi.com/?i=%s&apikey=6110282", id) // Establish URL
 
 	req, _ := http.NewRequest("GET", url, nil) // Establish Request.
 
 	res, err := http.DefaultClient.Do(req) // Aquire Response (or error)
-	
-	if(err != nil) { 					// Check for error.
+
+	if err != nil { // Check for error.
 		fmt.Println(err)
 	}
 
-	defer res.Body.Close() 				// Close the resonse.
+	defer res.Body.Close() // Close the resonse.
 
-	body, _ := ioutil.ReadAll(res.Body) 	// Convert HTML Reqest Body into string.
+	body, _ := ioutil.ReadAll(res.Body) // Convert HTML Reqest Body into string.
 
 	json.Unmarshal([]byte(body), &movieMap) // Decode JSON format into our map.
 
-	fmt.Println(string(movieMap["Title"])) // Test by printing out the title.
+	string_ := formatData(movieMap["Title"], movieMap["Year"], movieMap["Director"])
+
+	titles[index][0] = string_
 }
 
-func writeTo(_map map[string]string, filename string) {
-	
+func formatData(title string, year string, director string) string{
+	return fmt.Sprintf("%s,%s,%s", title, year, director)
+}
+
+func writeTo(data [][]string, filename string) {
+	csvFile, err := os.Create(fmt.Sprintf("%s.csv", filename))
+	defer csvFile.Close()
+
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	fileWriter := csv.NewWriter(csvFile)
+	fileWriter.Comma = '|'
+
+	fileWriter.WriteAll(data)
+
+	fileWriter.Flush()
 }
 
 func main() {
-	connectAndCollect()
+	findTitles()
 }
